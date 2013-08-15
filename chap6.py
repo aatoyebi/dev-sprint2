c508fc0a08c9800bef89bee8fdce64271f15c70b
Amaka Atoyebi <amaka.atoyebi@gmail.com>
2013-08-15 14:04:41 -0400

    Sprint 2 completed projects

--- chap6.py
+++ chap6.py
@@ -1,9 +1,45 @@
 # Enter your answrs for chapter 6 here
-# Name:
+# Name: Amaka Atoyebi
 
 
 # Ex. 6.6
+def first(word):
+	return word[0]
+
+def last(word):
+	return word[-1]
+
+def middle(word):
+	return word[1: -1]
+
+def is_palindrome(word):
+	if (first(word) == last(word)):
+		print first(word)
+		print middle(word)
+		print last(word)
+		return True
+		
+	else:
+		return False
 
 
+is_palindrome('redivider')
 
 # Ex 6.8
+def gcd(a, b):
+	r = a%b
+	
+	if (b==0):
+	 	return a
+
+	if (r==0):
+		return b
+
+	if (a<b):
+		return gcd(b, a)
+
+	else:	
+	 	print r
+	 	return gcd(b, r)
+
+gcd(120, 900)	 	
